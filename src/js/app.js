b874ef791c6f1d2385774fa112a0a14366b9d46f
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-10-08 21:48:46 -0500

    Tipos de datos y comentarios en javascript

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+// Comentarios en Javascript
+
+// Una sola línea
+
+/* 
+  Múltiples lineas
+*/
+
+/**
+ * Múltiples lineas con asteriscos que den formato
+ */
+
+ /**
+  * TIPOS DE DATOS 
+  */
+
+ // Numero
+ var numero = 10;
+
+ // String
+ var string = "Esto es un string";
+ var string2 = 'Esto es un string con comillas simples';
+ var string3 = `Esto es un string modernou`;
+
+ // Boolean
+ var boolean1 = true;
+ var boolean2 = false;
+
+ // Null 
+ var vacio = null;
+
+ // Udefined
+ var indefinido = undefined;
